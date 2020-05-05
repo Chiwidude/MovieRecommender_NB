bb08b64b50afb833cca29a6e9daff1cc87dbb325
Chiwidude <franalonzo96@gmail.com>
2020-05-05 10:50:17 -0600

    Initial Commit

---
+++ src/main/java/com/mycompany/aiproject/Movie.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.aiproject;
+
+/**
+ *
+ * @author panch
+ */
+public class Movie {
+    
+   boolean color;
+   String name;
+   String[] plot_keywords;
+   String[] genres;
+   String director;
+   int critics_reviews;
+   String Actor1;
+   String Actor2;   
+   double IMDB_Score;
+   int facebook_likes;
+   int year;
+   
+   
+    
+}
